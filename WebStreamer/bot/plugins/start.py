224b0e16e63ad9cb27644549ef7f94d70194098b
Selfie SD <webtamil0@gmail.com>
2021-12-29 21:47:59 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -8,13 +8,13 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(b, m):
-    await m.reply('__Hi, First send me a telegram file then wait some times i can give direct link to your file :)\n » Powered by @HARP_Tech__',
+    await m.reply('__Hi, First send me a telegram file then wait some times i can give direct link to your file :)\n » Powered by @groupdcbots',
                   reply_markup=InlineKeyboardMarkup(
                       [
                           [
                               InlineKeyboardButton(
                                   f'{emoji.STAR} Channel {emoji.STAR}',
-                                  url='https://telegram.me/Harp_tech'
+                                  url='https://telegram.me/Groupdcbots'
                               )
                           ]
                       ]
